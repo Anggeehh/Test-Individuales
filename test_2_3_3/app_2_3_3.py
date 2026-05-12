a8ebb4982148869ebcc1893e61fa2b3f8eafd2cb
Anggeehh <apinore0703@g.ducaand.es>
2026-05-12 09:54:27 +0200

    Añado todos los test individuales

---
+++ test_2_3_3/app_2_3_3.py
@@ -0,0 +1,14 @@
+# Importamos la función para manejar plantillas
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    # Buscamos el archivo 'index_2_3_3.html' dentro de la carpeta /templates
+    # También podemos pasar datos como variables (nombre="Angel")
+    return render_template("index_2_3_3.html")
+
+if __name__ == "__main__":
+   
+    app.run(debug=True) 
